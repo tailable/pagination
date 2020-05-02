b55a8daf1574b80bba90c4f77fe9c017f2483805
Dordoy <jamesdordoy@gmail.com>
2020-05-02 17:09:34 +0100

    0.1.8 renderless pagination for UI

--- src/App.vue
+++ src/App.vue
@@ -1,153 +1,153 @@
 <template>
-  <div id="app" class="flex flex-col max-w-3xl mx-auto my-2 bg-white">
-    <h2 class="my-2 text-center">Size sm</h2>
-    <pagination
-        :data="data"
-        size="small"
-        :showNumbers="true"
-        @pageChanged="pageChanged">
-    </pagination>
+    <div id="app" class="flex flex-col max-w-3xl mx-auto my-2 bg-white">
+        <h2 class="my-2 text-center">Size sm</h2>
+        <pagination
+            :data="data"
+            size="small"
+            :showNumbers="true"
+            @pageChanged="pageChanged">
+        </pagination>
 
-    <h2 class="my-2 text-center">Size md(default)</h2>
-    <pagination
-        :data="data"
-        :showNumbers="true"
-        @pageChanged="pageChanged">
-    </pagination>
+        <h2 class="my-2 text-center">Size md(default)</h2>
+        <pagination
+            :data="data"
+            :showNumbers="true"
+            @pageChanged="pageChanged">
+        </pagination>
 
-    <h2 class="my-2 text-center">Simple Size md(default)</h2>
-    <pagination
-        :data="data"
-        @pageChanged="pageChanged">
-    </pagination>
+        <h2 class="my-2 text-center">Simple Size md(default)</h2>
+        <pagination
+            :data="data"
+            @pageChanged="pageChanged">
+        </pagination>
 
-    <pagination
-        :data="data"
-        @pageChanged="pageChanged">
-        <div slot="next-button">
-            <svg fill="none" class="w-4 h-4" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
-                <path d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
-            </svg>
-        </div>
+        <pagination
+            :data="data"
+            @pageChanged="pageChanged">
+            <div slot="next-button">
+                <svg fill="none" class="w-4 h-4" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                    <path d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+                </svg>
+            </div>
 
-        <div slot="previous-button">
-            <svg fill="none" class="w-4 h-4" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
-                <path d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
-            </svg>
-        </div>
-    </pagination>
+            <div slot="previous-button">
+                <svg fill="none" class="w-4 h-4" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                    <path d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
+                </svg>
+            </div>
+        </pagination>
 
-    <pagination
-        :data="data"
-        @pageChanged="pageChanged">
-        <div slot="next-button">
-            Next
-        </div>
+        <pagination
+            :data="data"
+            @pageChanged="pageChanged">
+            <div slot="next-button">
+                Next
+            </div>
 
-        <div slot="previous-button">
-            Previous
-        </div>
-    </pagination>
+            <div slot="previous-button">
+                Previous
+            </div>
+        </pagination>
 
-    <h2 class="my-2 text-center">Simple Size sm</h2>
-    <pagination
-        :data="data"
-        size="small"
-        @pageChanged="pageChanged">
-    </pagination>
+        <h2 class="my-2 text-center">Simple Size sm</h2>
+        <pagination
+            :data="data"
+            size="small"
+            @pageChanged="pageChanged">
+        </pagination>
 
-     <pagination
-        :data="data"
-        size="small"
-        @pageChanged="pageChanged">
-        <div slot="next-button">
-            <svg fill="none" class="w-4 h-4" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
-                <path d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
-            </svg>
-        </div>
+        <pagination
+            :data="data"
+            size="small"
+            @pageChanged="pageChanged">
+            <div slot="next-button">
+                <svg fill="none" class="w-4 h-4" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                    <path d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+                </svg>
+            </div>
 
-        <div slot="previous-button">
-            <svg fill="none" class="w-4 h-4" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
-                <path d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
-            </svg>
-        </div>
-    </pagination>
+            <div slot="previous-button">
+                <svg fill="none" class="w-4 h-4" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                    <path d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
+                </svg>
+            </div>
+        </pagination>
 
-    <pagination
-        :data="data"
-        size="small"
-        @pageChanged="pageChanged">
-        <div slot="next-button">
-            Next
-        </div>
+        <pagination
+            :data="data"
+            size="small"
+            @pageChanged="pageChanged">
+            <div slot="next-button">
+                Next
+            </div>
 
-        <div slot="previous-button">
-            Previous
-        </div>
-    </pagination>
+            <div slot="previous-button">
+                Previous
+            </div>
+        </pagination>
 
-    <h2 class="my-2 text-center">Custom Body using Slot</h2>
-    <pagination
-        :data="data"
-        size="small"
-        @pageChanged="pageChanged">
+        <h2 class="my-2 text-center">Custom Body using Slot</h2>
+        <pagination
+            :data="data"
+            size="small"
+            @pageChanged="pageChanged">
 
-        <div
-            class="flex"
-            slot-scope="{ data, pageRange, currentPage, previousButtonHandler, nextButtonHandler, pageButtonHandler }">
-             <ul class="relative inline-flex list-none mx-auto mt-4">
-                <li>
-                    <button
-                        @click="previousButtonHandler"
-                        :disabled="!data.links.prev"
-                        class="inline-flex items-center px-2 py-2 h-full text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border-l border-t border-b border-gray-400 hover:bg-gray-200 active:bg-gray-200 focus:outline-none focus:border-blue-300 focus:shadow-outline active:text-gray-700 disabled:opacity-50 disabled:cursor-not-allowed">
-                        
-                        <svg
-                            fill="none"
-                            class="w-5 h-5"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24">
-                            <path d="M15 19l-7-7 7-7"></path>
-                        </svg>
-                    </button>
-                </li>
+            <div
+                class="flex"
+                slot-scope="{ data, pageRange, currentPage, previousButtonHandler, nextButtonHandler, pageButtonHandler }">
+                <ul class="relative inline-flex list-none mx-auto mt-4">
+                    <li>
+                        <button
+                            @click="previousButtonHandler"
+                            :disabled="!data.links.prev"
+                            class="inline-flex items-center px-2 py-2 h-full text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border-l border-t border-b border-gray-400 hover:bg-gray-200 active:bg-gray-200 focus:outline-none focus:border-blue-300 focus:shadow-outline active:text-gray-700 disabled:opacity-50 disabled:cursor-not-allowed">
+                            
+                            <svg
+                                fill="none"
+                                class="w-5 h-5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24">
+                                <path d="M15 19l-7-7 7-7"></path>
+                            </svg>
+                        </button>
+                    </li>
 
-                <li v-for="(page, key) in pageRange" :key="key">
-                    <button
-                        @click="pageButtonHandler(page)"
-                        :class="{
-                            'bg-purple-500 text-white': page == currentPage,
-                            'text-gray-700 border-l border-t border-b border-gray-400 hover:bg-gray-200': page != currentPage,
-                        }"
-                        class="inline-flex items-center px-4 py-2 h-full text-base font-medium leading-5 transition duration-150 ease-in-out bg-white active:bg-gray-200 focus:outline-none focus:border-blue-300 focus:shadow-outline active:text-gray-700">
-                        {{ page }}
-                    </button>
-                </li>
-                <li>
-                    <button
-                        @click="nextButtonHandler"
-                        :disabled="!data.links.next"
-                        class="inline-flex items-center px-2 py-2 h-full text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-400 hover:bg-gray-200 active:bg-gray-200 focus:outline-none focus:border-blue-300 focus:shadow-outline active:text-gray-700 disabled:opacity-50 disabled:cursor-not-allowed">
-                        
-                        <svg
-                            fill="none"
-                            class="w-5 h-5"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24">
-                            <path d="M9 5l7 7-7 7"></path>
-                        </svg>
-                    </button>
-                </li>
-            </ul>
-        </div>
-    </pagination>
-  </div>
+                    <li v-for="(page, key) in pageRange" :key="key">
+                        <button
+                            @click="pageButtonHandler(page)"
+                            :class="{
+                                'bg-purple-500 text-white': page == currentPage,
+                                'text-gray-700 border-l border-t border-b border-gray-400 hover:bg-gray-200': page != currentPage,
+                            }"
+                            class="inline-flex items-center px-4 py-2 h-full text-base font-medium leading-5 transition duration-150 ease-in-out bg-white active:bg-gray-200 focus:outline-none focus:border-blue-300 focus:shadow-outline active:text-gray-700">
+                            {{ page }}
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            @click="nextButtonHandler"
+                            :disabled="!data.links.next"
+                            class="inline-flex items-center px-2 py-2 h-full text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-400 hover:bg-gray-200 active:bg-gray-200 focus:outline-none focus:border-blue-300 focus:shadow-outline active:text-gray-700 disabled:opacity-50 disabled:cursor-not-allowed">
+                            
+                            <svg
+                                fill="none"
+                                class="w-5 h-5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24">
+                                <path d="M9 5l7 7-7 7"></path>
+                            </svg>
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </pagination>
+    </div>
 </template>
 
 <script lang="ts">
