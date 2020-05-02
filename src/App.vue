98dfccf18b13f90968730b3fe167ee4caa5ea655
Dordoy <jamesdordoy@gmail.com>
2020-05-02 16:19:23 +0100

    Added functionality to override main body slot for custom styling.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="flex flex-col max-w-3xl mx-auto my-2 bg-white">
     <h2 class="my-2 text-center">Size sm</h2>
     <pagination
         :data="data"
@@ -83,6 +83,66 @@
 
         <div slot="previous-button">
             Previous
+        </div>
+    </pagination>
+
+    <h2 class="my-2 text-center">Custom Body using Slot</h2>
+    <pagination
+        :data="data"
+        size="small"
+        @pageChanged="pageChanged">
+
+        <div class="flex" slot-scope="{ data, pageRange, currentPage, limit, showDisabled, size, computed, prevButtonEvents, nextButtonEvents, pageButtonEvents }">
+             <ul class="relative inline-flex list-none">
+                <li>
+                    <button
+                        v-on="prevButtonEvents"
+                        :disabled="!data.links.prev"
+                        class="inline-flex items-center px-2 py-2 h-full text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border-l border-t border-b border-gray-400 hover:bg-gray-200 active:bg-gray-200 focus:outline-none focus:border-blue-300 focus:shadow-outline active:text-gray-700 disabled:opacity-50 disabled:cursor-not-allowed">
+                        
+                        <svg
+                            fill="none"
+                            class="w-5 h-5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24">
+                            <path d="M15 19l-7-7 7-7"></path>
+                        </svg>
+                    </button>
+                </li>
+
+                <li v-for="(page, key) in pageRange" :key="key">
+                    <button
+                        v-on="pageButtonEvents(page)"
+                        :class="{
+                            'bg-purple-500 text-white': page == currentPage,
+                            'text-gray-700 border-l border-t border-b border-gray-400 hover:bg-gray-200': page != currentPage,
+                        }"
+                        class="inline-flex items-center px-4 py-2 h-full text-base font-medium leading-5 transition duration-150 ease-in-out bg-white active:bg-gray-200 focus:outline-none focus:border-blue-300 focus:shadow-outline active:text-gray-700">
+                        {{ page }}
+                    </button>
+                </li>
+                <li>
+                    <button
+                        v-on="nextButtonEvents"
+                        :disabled="!data.links.next"
+                        class="inline-flex items-center px-2 py-2 h-full text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-400 hover:bg-gray-200 active:bg-gray-200 focus:outline-none focus:border-blue-300 focus:shadow-outline active:text-gray-700 disabled:opacity-50 disabled:cursor-not-allowed">
+                        
+                        <svg
+                            fill="none"
+                            class="w-5 h-5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24">
+                            <path d="M9 5l7 7-7 7"></path>
+                        </svg>
+                    </button>
+                </li>
+            </ul>
         </div>
     </pagination>
   </div>
--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -7,110 +7,113 @@
         @pagination-change-page="onPaginationChangePage">
 
         <div
-            class="flex flex-col max-w-3xl mx-auto my-2 bg-white"
             slot-scope="{ data, limit, showDisabled, size, computed, prevButtonEvents, nextButtonEvents, pageButtonEvents }">
-            <template v-if="data.meta">
-                <div
-                    class="flex flex-col items-center justify-between px-4 py-3 border-t border-gray-400 sm:flex-row">
-                    <div class="flex mb-3 sm:mb-0">
-                        <p
-                            :class="{
-                                'text-base': size == 'default',
-                                'text-sm': size == 'small',
-                            }"
-                            class="leading-5 text-gray-700">
-                            Showing
-                            <span class="font-medium">{{ data.meta.from }}</span>
-                            to
-                            <span class="font-medium">{{ data.meta.to }}</span>
-                            of
-                            <span class="font-medium">{{ data.meta.total }}</span>
-                            results
-                        </p>
-                    </div>
-                    <div class="flex">
-                        <ul
-                            class="relative inline-flex list-none"
-                            v-if="computed.total > computed.perPage">
-                            <li
-                                v-if="computed.prevPageUrl || showDisabled">
-                                <button
-                                    type="button"
-                                    v-on="prevButtonEvents"
-                                    :tabindex="!computed.prevPageUrl && -1"
-                                    :class="{
-                                        'px-2 py-2': size == 'default',
-                                        'px-1 py-1': size == 'small',
-                                    }"
-                                    :disabled="!computed.prevPageUrl"
-                                    class="inline-flex items-center h-full mx-1 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-400 rounded-md hover:bg-gray-200 active:bg-gray-200 focus:outline-none focus:border-blue-300 focus:shadow-outline active:text-gray-700 disabled:opacity-50 disabled:cursor-not-allowed">
-                                    
-                                    <slot name="previous-button">
-                                        <svg fill="none"
-                                            :class="{
-                                                'w-5 h-5': size == 'default',
-                                                'w-4 h-4': size == 'small',
-                                            }"
-                                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
-                                            <path d="M15 19l-7-7 7-7"></path>
-                                        </svg>
-                                    </slot>
-                                </button>
-                            </li>
+            <slot
+                :data="data"
+                :currentPage="computed.currentPage"
+                :pageRange="computed.pageRange"
+                :prevButtonEvents="prevButtonEvents"
+                :nextButtonEvents="nextButtonEvents"
+                :pageButtonEvents="pageButtonEvents">
 
-                            <template v-if="showNumbers">
-                                <li  v-for="(page, key) in computed.pageRange" :key="key">
-                                    <button type="button"
+                <template v-if="data.meta">
+                    <div
+                        class="flex flex-col items-center justify-between px-4 py-3 border-t border-gray-400 sm:flex-row">
+                        <div class="flex mb-3 sm:mb-0">
+                            <p
+                                :class="{
+                                    'text-base': size == 'default',
+                                    'text-sm': size == 'small',
+                                }"
+                                class="leading-5 text-gray-700">
+                                Showing
+                                <span class="font-medium">{{ data.meta.from }}</span>
+                                to
+                                <span class="font-medium">{{ data.meta.to }}</span>
+                                of
+                                <span class="font-medium">{{ data.meta.total }}</span>
+                                results
+                            </p>
+                        </div>
+                        <div class="flex">
+                            <ul
+                                class="relative inline-flex list-none"
+                                v-if="computed.total > computed.perPage">
+                                <li
+                                    v-if="computed.prevPageUrl || showDisabled">
+                                    <button
+                                        type="button"
+                                        v-on="prevButtonEvents"
+                                        :tabindex="!computed.prevPageUrl && -1"
                                         :class="{
-                                            'bg-blue-500': page == computed.currentPage,
-                                            'text-white': page == computed.currentPage,
-                                            'text-gray-700': page != computed.currentPage,
-                                            'hover:bg-gray-200': page != computed.currentPage,
-                                            'border': page != computed.currentPage,
-                                            'border-gray-400': page != computed.currentPage,
-                                            'px-4': size == 'default',
-                                            'py-2': size == 'default',
-                                            'px-2': size == 'small',
-                                            'py-1': size == 'small',
+                                            'px-2 py-2': size == 'default',
+                                            'px-1 py-1': size == 'small',
                                         }"
-                                        v-on="pageButtonEvents(page)"
-                                        class="inline-flex items-center h-full mx-1 text-base font-medium leading-5 transition duration-150 ease-in-out bg-white rounded-md active:bg-gray-200 focus:outline-none focus:border-blue-300 focus:shadow-outline active:text-gray-700">
-                                        {{ page }}
-                                        <span class="sr-only" v-if="page == computed.currentPage">(current)</span>
+                                        :disabled="!computed.prevPageUrl"
+                                        class="inline-flex items-center h-full mx-1 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-400 rounded-md hover:bg-gray-200 active:bg-gray-200 focus:outline-none focus:border-blue-300 focus:shadow-outline active:text-gray-700 disabled:opacity-50 disabled:cursor-not-allowed">
+                                        
+                                        <slot name="previous-button">
+                                            <svg fill="none"
+                                                :class="{
+                                                    'w-5 h-5': size == 'default',
+                                                    'w-4 h-4': size == 'small',
+                                                }"
+                                                stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                                                <path d="M15 19l-7-7 7-7"></path>
+                                            </svg>
+                                        </slot>
                                     </button>
                                 </li>
-                            </template>
 
-                            <li
-                                v-if="computed.nextPageUrl || showDisabled">
-                                <button
-                                    type="button"
-                                    :tabindex="!computed.nextPageUrl && -1"
-                                    v-on="nextButtonEvents"
-                                    :class="{
-                                        'px-2 py-2': size == 'default',
-                                        'px-1 py-1': size == 'small',
-                                    }"
-                                    :disabled="!computed.nextPageUrl"
-                                    class="inline-flex items-center h-full mx-1 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-400 rounded-md hover:bg-gray-200 active:bg-gray-200 focus:outline-none focus:border-blue-300 focus:shadow-outline active:text-gray-700 disabled:opacity-50 disabled:cursor-not-allowed">
-                                    
-                                    <slot name="next-button">
-                                        <svg
-                                            fill="none"
+                                <template v-if="showNumbers">
+                                    <li  v-for="(page, key) in computed.pageRange" :key="key">
+                                        <button
+                                            type="button"
+                                            v-on="pageButtonEvents(page)"
                                             :class="{
-                                                'w-5 h-5': size == 'default',
-                                                'w-4 h-4': size == 'small',
+                                                'px-2 py-1': size == 'small',
+                                                'px-4 py-2': size == 'default',
+                                                'bg-blue-500 text-white': page == computed.currentPage,
+                                                'text-gray-700 border border-gray-400 hover:bg-gray-200': page != computed.currentPage,
                                             }"
-                                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
-                                            <path d="M9 5l7 7-7 7"></path>
-                                        </svg>
-                                    </slot>
-                                </button>
-                            </li>
-                        </ul>
+                                            class="inline-flex items-center h-full mx-1 text-base font-medium leading-5 transition duration-150 ease-in-out bg-white rounded-md active:bg-gray-200 focus:outline-none focus:border-blue-300 focus:shadow-outline active:text-gray-700">
+                                            {{ page }}
+                                            <span class="sr-only" v-if="page == computed.currentPage">(current)</span>
+                                        </button>
+                                    </li>
+                                </template>
+
+                                <li
+                                    v-if="computed.nextPageUrl || showDisabled">
+                                    <button
+                                        type="button"
+                                        :tabindex="!computed.nextPageUrl && -1"
+                                        v-on="nextButtonEvents"
+                                        :class="{
+                                            'px-2 py-2': size == 'default',
+                                            'px-1 py-1': size == 'small',
+                                        }"
+                                        :disabled="!computed.nextPageUrl"
+                                        class="inline-flex items-center h-full mx-1 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-400 rounded-md hover:bg-gray-200 active:bg-gray-200 focus:outline-none focus:border-blue-300 focus:shadow-outline active:text-gray-700 disabled:opacity-50 disabled:cursor-not-allowed">
+                                        
+                                        <slot name="next-button">
+                                            <svg
+                                                fill="none"
+                                                :class="{
+                                                    'w-5 h-5': size == 'default',
+                                                    'w-4 h-4': size == 'small',
+                                                }"
+                                                stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24">
+                                                <path d="M9 5l7 7-7 7"></path>
+                                            </svg>
+                                        </slot>
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </template>
+                </template>
+            </slot>
         </div>
     </renderless-pagination>
 </template>
