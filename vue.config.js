100579277034b8f4e2a750ef4bb6f6a6fc889f22
James Dordoy <jamesdordoy@gmail.com>
2022-05-27 17:17:49 +0100

    Update node version

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,5 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
     ? '/pagination/'
     : '/',
-    // outputDir: path.resolve(__dirname, "./docs"),
+    outputDir: path.resolve(__dirname, "./docs"),
 }
